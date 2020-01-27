bb8cf006b754f4e4b9c3a128a4169c5526248ff5
Ivan Holovach <igolovac629@gmail.com>
2020-01-27 18:58:33 +0200

    UFO created seperated repo for the api

--- modules/request_controllers/__init__.py
+++ modules/request_controllers/__init__.py
@@ -1,3 +1,3 @@
-from .ufo_items_controller import UfoItems
 from .ufo_chart_controller import UfoChart
+from .ufo_items_controller import UfoItems
 from .ufo_request_controller_base import BaseController
